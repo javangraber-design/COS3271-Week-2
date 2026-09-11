df64498a6ee6beb02b807420a8e843fe6c8af671
Javan Graber <jmgraber@students.unwsp.edu>
2026-09-11 16:37:47 -0500

    Update Program #2 Name APP.java

--- Program #2 Name APP.java
+++ Program #2 Name APP.java
@@ -1,5 +1,5 @@
 //Programmer: Javan Graber
-//Date: 9/9/26
+//Date: 9/11/26
 
 package javanproject;
 
@@ -26,11 +26,13 @@ public class MyProject {
 		
 		//Print the info
 		System.out.println("\n");
-		System.out.println("Here is the information you typed organized in a neat fashion:\n");
-		System.out.print("\tFirst name: " + firstName + "\n");
-		System.out.print("\tLast name: " + lastName + "\n");
-		System.out.print("\tAge: " + age + "\n");
-		System.out.format("\tAverage hours of sleep	: %.1f", + sleepHours);
+		System.out.format("%65s", "Here is the information you typed organized in a neat fashion:");
+		System.out.println("\n");
+		System.out.println("\tFirst name: " + firstName + "\n");
+		System.out.println("\tLast name: " + lastName + "\n");
+		System.out.println("\tAge: " + age + "\n");
+		System.out.format("\tAverage hours of sleep: %.1f", + sleepHours);
+		System.out.println("\n");
 		
 	}
 
